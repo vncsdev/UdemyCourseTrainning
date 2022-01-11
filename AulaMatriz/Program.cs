f7cf977ec5ce901d389aae79d76c5fdc3663884c
Vinicius <vinicius.gabriel83@yahoo.com>
2022-01-11 19:34:54 -0300

    inicio matriz

---
+++ AulaMatriz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AulaMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++) // linhas
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++) // colunas
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+        }
+    }
+}
