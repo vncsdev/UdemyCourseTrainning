f3cf688d8b0f0934b7254eb5bf4c86d8fd38dc5f
Vinicius Gabriel <vinicius.gabriel83@yahoo.com>
2022-01-11 21:38:51 -0300

    matriz ok

--- AulaMatriz/Program.cs
+++ AulaMatriz/Program.cs
@@ -19,6 +19,26 @@ namespace AulaMatriz
                     mat[i, j] = int.Parse(values[j]);
                 }
             }
+
+            Console.Write("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Negative numbers: {count}");
         }
     }
 }
---
+++ ExMatriz/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ExMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nº de linhas: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Nº de colunas: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.Write("Digite um número: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine($"Position {i}, {j}:");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
