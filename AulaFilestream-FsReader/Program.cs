af9710db697df648e965e6306dc0e4661bb590e8
Vinicius <vinicius.gabriel83@yahoo.com>
2022-01-25 16:40:54 -0300

    inicio files

---
+++ AulaFilestream-FsReader/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace AulaFilestream_FsReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
