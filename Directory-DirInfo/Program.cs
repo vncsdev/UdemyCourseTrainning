7c9751ed2264b9170263a44a889db720b1b8e754
Vinicius Gabriel <vinicius.gabriel83@yahoo.com>
2022-01-25 21:44:19 -0300

    cont files

---
+++ Directory-DirInfo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Directory_DirInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
